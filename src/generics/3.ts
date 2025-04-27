710689f1482df03bb67486165b77a40d824caa3a
MykytaKononov <nikitamaxkononov@gmail.com>
2025-04-27 18:19:17 +0200

    fix

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,5 +1,5 @@
-function merge<A, B>(objA: A, objB: B) {
-  return Object.assign(objA, objB);
+function merge<A, B>(objA: A, objB: B): A & B {
+  return Object.assign({}, objA, objB);
 }
 
 const mergedObj = merge({ name: "Max" }, { age: 30 });
